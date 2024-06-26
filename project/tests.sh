060093febc0cb3e9a9db80ab051bdd1784e31906
Mahshidaf <144285171+Mahshidaf@users.noreply.github.com>
2024-06-26 20:53:38 +0200

    Update tests.sh

--- project/tests.sh
+++ project/tests.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-python3 /project/tests.py
 
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+echo "Running tests..."
+
+# Assuming tests.sh is called from the root directory and tests.py is inside 'project'
+pytest project/tests.py
